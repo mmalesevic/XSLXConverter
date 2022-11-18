a7ed519df80460b17cef3e8d047bfa3c238e4a08
Matej Malesevic <matej.malesevic@approppo.ch>
2022-11-18 15:49:28 +0100

    Initial Commit

---
+++ xslx to json/xslx_to_jsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  xslx_to_jsonApp.swift
+//  xslx to json
+//
+//  Created by Matej Malesevic on 18.11.22.
+//
+
+import SwiftUI
+
+@main
+struct xslx_to_jsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xslx to jsonUITests/xslx_to_jsonUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  xslx_to_jsonUITestsLaunchTests.swift
+//  xslx to jsonUITests
+//
+//  Created by Matej Malesevic on 18.11.22.
+//
+
+import XCTest
+
+final class xslx_to_jsonUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
